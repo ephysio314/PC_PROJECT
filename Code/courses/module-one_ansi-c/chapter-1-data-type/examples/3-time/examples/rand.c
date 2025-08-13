1fd24c450418278ee5b0c1a0b3ba4eac87326526
ephysio314 <80077020+ephysio314@users.noreply.github.com>
2025-08-13 15:43:04 +0200

    Add files via upload

--- Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/rand.c
+++ Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/rand.c
@@ -8,9 +8,43 @@ Tuto
 13 min, LearningLad, How to Generate Random Numbers in C | Using rand(), srand(), and time() for Unique Sequences:
 https://www.youtube.com/watch?v=oXEDMNXzuo4
 
+https://www.cmastery.digibeatrix.com/en/standard-library-functions/c-language-random-generation-methods/
 
+https://thelinuxcode.com/rand-function-in-c-language/
 
+https://www.cmastery.digibeatrix.com/en/standard-library-functions/c-language-random-number-rand-srand/
 
+https://codeforces.com/blog/entry/61587
+
+source code of rand example:
+https://stackoverflow.com/questions/1026327/what-common-algorithms-are-used-for-cs-rand
+
+~{
+int
+rand_r (unsigned int *seed)
+{
+  unsigned int next = *seed;
+  int result;
+
+  next *= 1103515245;
+  next += 12345;
+  result = (unsigned int) (next / 65536) % 2048;
+
+  next *= 1103515245;
+  next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (next / 65536) % 1024;
+
+  next *= 1103515245;
+  next += 12345;
+  result <<= 10;
+  result ^= (unsigned int) (next / 65536) % 1024;
+
+  *seed = next;
+
+  return result;
+}
+}~
 */
 
 #include <stdio.h> /* printf */
@@ -19,6 +53,7 @@ https://www.youtube.com/watch?v=oXEDMNXzuo4
 #include <limits.h> /* SCHAR_MAX */
 #include <stdbool.h>
 #include <string.h> /* memset */
+#include <time.h>
 
 #include <mytime.h>
 /*
@@ -30,200 +65,263 @@ long time_Microsec(void);
 float time_Float(void);
 */
 
+
 /*
-~DEBUG:
-	A) Pair array buffer
-	
-	We have two fix-size array integer of same size.
-	They will be fill using the speudorandom number generater
-	each by one process.
-	
-	Meaning that we call the generator to fill one.
-	And we call a second time the generator to fill the second.
-	
-	In a third integer array with the same size than the others.
-	
-	We iteratate the two array and compare if was the same value.
-	If was we store one in the third, else zero.
-	
-	We ouput in the shell who many are similar.
-	
-	Optionnal:
-	  And in a text file the third array result.
-	 
+~TEST
+
+A) Methode
+
+The test was a function-module.
+
+It have he's encapsulate methodes and values.
+
+It will be varing using a modulare way
+for the feature we want to test.
+
+It access function needed for the featureusage.
+They could be to access data.
+
+Those function will be call using an interface function
+that can call all the version of the function.
+
+Example:
+~{
+  enum e_module_srand{
+  	E_MODULE_SRAND_ONE
+  	E_MODULE_SRAND_TWO
+  }
+  
+  void call_Srand(enum e_module_srand _id){
+  	switch(_id){
+  		case E_MODULE_SRAND_ONE:
+  			module_one_Srand();
+  			break;
+  	
+  		case E_MODULE_SRAND_TWO:
+  			module_two_Srand();
+  			break;
+  	}
+  }
+}~
+
+B) Tests
+
+1) Similitude test
+
+Have two array of same fix-len.
+Each are fill with a random number generator process
+with the same methode but one after the other.
+
+Using a third array of same len
+of type boolean.
+Compare each array.
+Store the result of equality condition
+in this third corresponding index.
+
+Count hte number of time the third array
+have a true value in it.
+
 */
 
 /*
-~DEBUG
+~RANDOM-GLOBAL
 */
 
-bool buff_one_and_two_IsFill(void);
-bool buff_three_IsFill(void);
-int buff_GetId(void);
-void buff_Reset(void);
-bool buff_Push(const char _val);
-bool buff_three_Upd(void);
-int buff_count_GetSimilar(void);
-bool buff_three_Print(void);
+/*
+Random parameters:
+- seed. ( internaly generated from the module local scope )
+- rand-return value. ( Set using the module return and get for the call-function return. This emulate the rand function. )
+*/
+
+static int buff_rand=0;
+
+void buff_rand_Set(const int _val){
+	buff_rand=_val;
+}
+const int buff_rand_Get(void){
+	return buff_rand;
+}
+
+/*
+~MODULES
+*/
+
+enum e_module_random{
+	E_MODULE_RANDOM_NULL,
+	
+	E_MODULE_RANDOM_ONE,
+	E_MODULE_RANDOM_TWO,
+	
+	E_MODULE_RANDOM_MAX
+};
+
+void module_random_one_Srand(void);
+void module_random_two_Srand(void);
+
+void module_random_CallSrand(enum e_module_random _module_id){
+	switch(_module_id){
+		case E_MODULE_RANDOM_ONE:
+			module_random_one_Srand();
+			break;
+
+		case E_MODULE_RANDOM_TWO:
+		  module_random_two_Srand();
+			break;
+	
+		default:
+			break;
+	}
+}
+
+void module_random_one_Rand(void);
+void module_random_two_Rand(void);
+
+const int module_random_CallRand(enum e_module_random _module_id){
+	switch(_module_id){
+		case E_MODULE_RANDOM_ONE:
+			module_random_one_Rand();
+			break;
+
+		case E_MODULE_RANDOM_TWO:
+			module_random_two_Rand();
+			break;
+	
+		default:
+			break;
+	}
+	return (const int)buff_rand_Get();
+}
+
+/*
+~SRAND
+*/
+
+void module_random_one_Srand(void){
+	srand(time(NULL));
+}
+
+void module_random_two_Srand(void){
+	srand(time(NULL));
+}
+
+/*
+~RAND
+*/
+
+void module_random_one_Rand(void){
+	int r=0;
+	r=rand()%INT_MAX;
+	buff_rand_Set(r);
+}
+
+void module_random_two_Rand(void){
+	int r=0;
+	r=rand()%INT_MAX;
+	buff_rand_Set(r);
+}
+
+/*
+~TEST-ZERO ( system test )
+*/
+
+void test_zero(void){
+	printf("Call: '%s'\n", __FUNCTION__);
+	
+	const char descr[]=
+	"Descr:\n"
+	"  Test the function that call each modules\n"
+	"  in he's most basic form.\n";
+
+	printf(descr);
+
+	int k=E_MODULE_RANDOM_ONE;
+	int kmax=E_MODULE_RANDOM_MAX;
+	while(k < kmax){
+
+		/* SETUP */
+	
+  	enum e_module_random module_id = (enum e_module_random)k;
+
+		printf("Setup: module id: '%d'\n", module_id);
+	
+		/* SEED */
+
+		printf("Seed\n");
+
+		module_random_CallSrand(module_id);
+	
+		/* RAND */
+		
+		printf("Generating\n");
+	
+		int i=0;
+		int imax=20;
+		while(i < imax){
+			printf("i '%d' r '%d'\n", i, module_random_CallRand(module_id));
+			i++;
+		}
+		
+		/* END */
+	
+		k++;
+	}
+}
+
+/*
+~TEST-ONE ( similitude bettween two iteration )
+*/
 
 #define BUFF_MAX 100
 
-static int buff_id_select=0;
-
-int buff_one[BUFF_MAX] = (int[BUFF_MAX]){0};
-int buff_one_fill=0;
-
-int buff_two[BUFF_MAX] = (int[BUFF_MAX]){0};
-int buff_two_fill=0;
-
-bool buff_three[BUFF_MAX] = (bool[BUFF_MAX]){0};
-int buff_three_fill=0;
-
-void buff_Reset(void){
-	memset(buff_one, 0, sizeof(int) * BUFF_MAX);
-	buff_one_fill=0;
-	
-	memset(buff_two, 0, sizeof(int) * BUFF_MAX);
-	buff_two_fill=0;
-	
-	memset(buff_three, 0, sizeof(bool) * BUFF_MAX);
-	buff_three_fill=0;
-}
-
-int buff_GetId(void){
-	return buff_id_select;
-}
-
-bool buff_one_and_two_IsFill(void){
-  return (bool)(BUFF_MAX == buff_one_fill && BUFF_MAX == buff_two_fill);
-}
-bool buff_three_IsFill(void){
-  return (bool)(BUFF_MAX == buff_three_fill);
-}
-
-bool buff_Push(const char _val){
-	switch(buff_id_select){
-		case 0:
-			if(buff_one_fill < BUFF_MAX){
-		    buff_one[buff_one_fill]=_val;
-				buff_one_fill++;
-				break;
-			}else{
-				buff_id_select++;
-			}
-			
-		case 1:
-			if(buff_two_fill < BUFF_MAX){
-		    buff_two[buff_two_fill]=_val;
-				buff_two_fill++;
-			}else{
-				buff_id_select++;
-			}
-			break;
-		case 2:
-			buff_three_Upd();
-			break;
-			
-		default:
-			return 1;
-	}
-	return 0;
-}
-
-bool buff_three_Upd(void){
-	if(!buff_one_and_two_IsFill()){
-		printf("Warning: Buff one and two is not fill!\n");return 1;
-	}
-
-	int i=0;
-	int imax=BUFF_MAX;
-	while(i < imax){
-		buff_three[i] = (bool)(buff_one[i] == buff_two[i]);
-		buff_three_fill++;
-		i++;
-	}
-
-	return 0;
-}
-
-int buff_count_GetSimilar(void){
-	if(BUFF_MAX != buff_three_fill-1){ return -1; }
-	
-	buff_three_Upd();
-	
-	int return_val=0;
-	
-	int i=0;
-	int imax=BUFF_MAX;
-	while(i < imax){
-		buff_three[i] = (bool)(buff_one[i] == buff_two[i]);
-		i++;
-	}
-	return return_val;
-}
-
-bool buff_three_Print(void){
-	buff_three_Upd();
-	if(!buff_three_IsFill()){
-		return 1;
-	}
-
-	int i=0;
-	int imax=BUFF_MAX;
-	while(i < imax){
-		printf("buff_three['%d']='%d'", i, buff_three[i]);
-		i++;
-	}
-	
-	int similar_count = buff_count_GetSimilar();
-	if(-1 == buff_three_Print()){ printf("Warning: fail to call buff_count_GetSimilar!\n"); return 1; }
-	printf("Similar: '%d'\n", similar_count);
-
-	return 0;
-}
-
-/*
-~UNIT-TEST
-*/
-
-void out_rand_one(void){
-	printf("Call: '%s'\n", __FUNCTION__);
-	int i;
-	
-	buff_Reset();
-
-	i=0;
-	printf("buff id '%d'\n", buff_GetId());
-	while(0 == buff_GetId()){
-		if(BUFF_MAX <= i){ printf("Loop using id break because i edge reach\n");break; }
-		int r=rand();
-		buff_Push(r);
-		printf("i '%d', r '%d'\n", i, r);
-		i++;
-	}
-	
-	i=0;
-	printf("buff id '%d'\n", buff_GetId());
-	while(1 == buff_GetId()){
-		if(BUFF_MAX <= i){ printf("Loop using id break because i edge reach\n");break; }
-		int r=rand();
-		buff_Push(r);
-		printf("i '%d', r '%d'\n", i, r);
-		i++;
-	}
-	
-	printf("buff id '%d'\n", buff_GetId());
-	if(0 != buff_three_Print()){ printf("Warning: fail to call buff_three_Print!\n"); }
-}
+static int buff_one[BUFF_MAX];
+static int buff_two[BUFF_MAX];
+static int buff_three[BUFF_MAX];
 
 void test_one(void){
-
-	printf("random val from '%d' to '%d'\n", 0, RAND_MAX);
+	printf("Call: '%s'\n", __FUNCTION__);
 	
-	out_rand_one();
+	const char descr[]=
+	"Descr:\n"
+	"  Two generating with a same module.\n"
+	"  Compare if the value for a same iteration are the same.\n";
+
+	printf(descr);
+
+	int k=E_MODULE_RANDOM_ONE;
+	int kmax=E_MODULE_RANDOM_MAX;
+	while(k < kmax){
+
+		/* SETUP */
+	
+  	enum e_module_random module_id = (enum e_module_random)k;
+
+		printf("Setup: module id: '%d'\n", module_id);
+	
+		/* SEED */
+
+		printf("Seed\n");
+
+		module_random_CallSrand(module_id);
+	
+		/* RAND */
+		
+		printf("Generating\n");
+	
+		int i=0;
+		int imax=20;
+		while(i < imax){
+			printf("i '%d' r '%d'\n", i, module_random_CallRand(module_id));
+			i++;
+		} /* i < imax */
+		
+		/* END */
+	
+		k++;
+	} /* k < kmax */
 }
 
 int main(void){
+	test_zero();
 	test_one();
 	return 0;
 }
