b7a721ea60d11895ac10c0143094ae4c29d70223
ephysio314 <80077020+ephysio314@users.noreply.github.com>
2025-08-13 19:42:55 +0200

    Add files via upload

---
+++ Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/magic.c
@@ -0,0 +1,21 @@
+
+/*
+~DESCRIPTION:
+	- Generate from a range a random number.
+	- Show a message that ask the user to enter a number in the range.
+	- Check three condition and return a message corresponding.
+	If was equal, congratule it and enter in the game-state of score-menu.
+	else, specify to the user if it's lower or greater,
+	and increase the number f try of this session.
+	- When reach the game-state score-menu, if was a success ( only possibility here)
+	increase he's score by one. Check the number of try
+	this game and the lower register and if lower than it store it.
+	Ask to enter 'Y' to replay and 'N' to exit.
+	If enter 'N', break the main loop.
+	If enter 'Y', call the game main-loop.
+*/
+
+int main(void){
+
+	return 0;
+}
