7de6ac4eb1adcb497f33797a8a6586779bade075
ephysio314 <80077020+ephysio314@users.noreply.github.com>
2025-08-13 11:38:39 +0200

    Add files via upload

--- Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/delay.c
+++ Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/delay.c
@@ -3,6 +3,14 @@
 #include <limits.h> /* INT_MAX */
 
 #include <mytime.h>
+/*
+char time_IsInit(void);
+void time_Start(void);
+void time_Upd(void);
+long time_Sec(void);
+long time_Microsec(void);
+float time_Float(void);
+*/
 
 void foo(void){
 	int i=0;
--- Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/rand.c
+++ Code/courses/module-one_ansi-c/chapter-1-data-type/examples/3-time/examples/rand.c
@@ -1,79 +1,215 @@
 
 /*
 https://www.man7.org/linux/man-pages/man2/gettimeofday.2.html
+
+
+Tuto
+
+13 min, LearningLad, How to Generate Random Numbers in C | Using rand(), srand(), and time() for Unique Sequences:
+https://www.youtube.com/watch?v=oXEDMNXzuo4
+
+
+
+
 */
 
 #include <stdio.h> /* printf */
+#include <stdlib.h> /* srand, rand, RAND_MAX */
 #include <stddef.h> /* NULL */
 #include <limits.h> /* SCHAR_MAX */
-#include <sys/time.h>
+#include <stdbool.h>
+#include <string.h> /* memset */
+
+#include <mytime.h>
 /*
-struct timeval{
-	time_t tv_sec; // seconds
-	suseconds_t tv_usec; // microseconds
-};
-gettimeofday()
+char time_IsInit(void);
+void time_Start(void);
+void time_Upd(void);
+long time_Sec(void);
+long time_Microsec(void);
+float time_Float(void);
 */
 
-static struct timeval t_start={0, 0};
-static struct timeval t={0, 0};
-static char t_is_init=0;
-
-char time_IsInit(void){
-	return t_is_init;
-}
-
-void time_Start(void){
-	gettimeofday(&t_start, NULL);
-	gettimeofday(&t, NULL);
-	t_is_init=1;
-}
-void time_Upd(void){
-	gettimeofday(&t, NULL);
-}
-
-int time_Sec(void){
-	return t.tv_sec - t_start.tv_sec;
-}
-int time_Microsec(void){
-	return t.tv_usec - t_start.tv_usec;
-}
-
-static int rand_buff[SCHAR_MAX] = (int[SCHAR_MAX]){0};
-static int rand_buff_fill=0;
-
-void rand_Load(void){
-	if(!time_IsInit()){ time_Start(); while(time_Sec() < 1 && time_Microsec() < 25){ time_Upd(); } }
-	time_Upd();
+/*
+~DEBUG:
+	A) Pair array buffer
 	
-	char need_seed=1;
-	int j=0;
-	int imax=SCHAR_MAX;
-	while(rand_buff_fill < imax){
-		if(j < 10){ j++; }
-		else{ j=0; need_seed=1; }
-		
-		if(need_seed){ time_Upd(); srand(time_Sec() * time_Microsec() + rand_buff_fill); need_seed=0; }
-		
-		rand_buff[rand_buff_fill]=rand()%INT_MAX + 1;
-		rand_buff_fill++;
-	}
+	We have two fix-size array integer of same size.
+	They will be fill using the speudorandom number generater
+	each by one process.
+	
+	Meaning that we call the generator to fill one.
+	And we call a second time the generator to fill the second.
+	
+	In a third integer array with the same size than the others.
+	
+	We iteratate the two array and compare if was the same value.
+	If was we store one in the third, else zero.
+	
+	We ouput in the shell who many are similar.
+	
+	Optionnal:
+	  And in a text file the third array result.
+	 
+*/
+
+/*
+~DEBUG
+*/
+
+void buff_Reset(void);
+int buff_GetId(void);
+bool buff_Push(const char _val);
+bool buff_three_Upd(void);
+int buff_count_GetSimilar(void);
+bool buff_three_Print(void);
+
+#define BUFF_MAX 100
+
+static int buff_id_select=0;
+
+int buff_one[BUFF_MAX] = (int[BUFF_MAX]){0};
+int buff_one_fill=0;
+
+int buff_two[BUFF_MAX] = (int[BUFF_MAX]){0};
+int buff_two_fill=0;
+
+bool buff_three[BUFF_MAX] = (bool[BUFF_MAX]){0};
+int buff_three_fill=0;
+
+void buff_Reset(void){
+	memset(buff_one, 0, sizeof(int) * BUFF_MAX);
+	buff_one_fill=0;
+	
+	memset(buff_two, 0, sizeof(int) * BUFF_MAX);
+	buff_two_fill=0;
+	
+	memset(buff_three, 0, sizeof(bool) * BUFF_MAX);
+	buff_three_fill=0;
 }
 
-int rand_Pop(void){
+int buff_GetId(void){
+	return buff_id_select;
+}
+
+bool buff_Push(const char _val){
+	switch(buff_id_select){
+		case 0:
+			if(buff_one_fill < BUFF_MAX){
+		    buff_one[buff_one_fill]=_val;
+				buff_one_fill++;
+				break;
+			}else{
+				buff_id_select++;
+			}
+			
+		case 1:
+			if(buff_two_fill < BUFF_MAX){
+		    buff_two[buff_two_fill]=_val;
+				buff_two_fill++;
+			}else{
+				buff_id_select++;
+			}
+			break;
+		case 2:
+			buff_three_Upd();
+			break;
+			
+		default:
+			return 1;
+	}
+	return 0;
+}
+
+bool buff_three_Upd(void){
+	if(BUFF_MAX != buff_one_fill-1 || BUFF_MAX != buff_two_fill-1){
+		return 1;
+	}
+
+	while(buff_three_fill < BUFF_MAX){
+		buff_three[buff_three_fill] = (bool)(buff_one[buff_three_fill] == buff_two[buff_three_fill]);
+		buff_three_fill++;
+	}
+
+	return 0;
+}
+
+int buff_count_GetSimilar(void){
+	if(BUFF_MAX != buff_three_fill-1){ return -1; }
+	
+	buff_three_Upd();
+	
 	int return_val=0;
-	if(rand_buff_fill <= 0){ rand_Load(); }
-	return_val = rand_buff[rand_buff_fill];
-	rand_buff_fill--;
+	
+	int i=0;
+	int imax=BUFF_MAX;
+	while(i < imax){
+		buff_three[i] = (bool)(buff_one[i] == buff_two[i]);
+		i++;
+	}
 	return return_val;
 }
 
-int main(void){
+bool buff_three_Print(void){
+	if(BUFF_MAX != buff_three_fill-1){
+		return 1;
+	}
+
+	buff_three_Upd();
+	
 	int i=0;
-	int imax=200;
+	int imax=BUFF_MAX;
 	while(i < imax){
-		printf("i '%d': rand '%d'\n", i, rand_Pop());
+		printf("buff_three['%d']='%d'", i, buff_three[i]);
 		i++;
 	}
+	
+	int similar_count = buff_count_GetSimilar();
+	if(-1 == buff_three_Print()){ printf("Warning: fail to call buff_count_GetSimilar!\n"); }
+	printf("Similar: '%d'\n", similar_count);
+
+	return 0;
+}
+
+/*
+~UNIT-TEST
+*/
+
+void out_rand_one(void){
+	printf("Call: '%s'\n", __FUNCTION__);
+	int i;
+	
+	buff_Reset();
+
+	i=0;
+	printf("buff id '%d'\n", buff_GetId());
+	while(0 == buff_GetId()){
+		int r=rand();
+		buff_Push(r);
+		printf("i '%d', r '%d'\n", i, r);
+		i++;
+	}
+	
+	i=0;
+	printf("buff id '%d'\n", buff_GetId());
+	while(1 == buff_GetId()){
+		int r=rand();
+		buff_Push(r);
+		printf("i '%d', r '%d'\n", i, r);
+		i++;
+	}
+	
+	if(0 != buff_three_Print()){ printf("Warning: fail to call buff_three_Print!\n"); }
+}
+
+void test_one(void){
+
+	printf("random val from '%d' to '%d'\n", 0, RAND_MAX);
+	
+	out_rand_one();
+}
+
+int main(void){
+	test_one();
 	return 0;
 }
