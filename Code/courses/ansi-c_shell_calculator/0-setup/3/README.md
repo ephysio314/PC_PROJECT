7587b6cb1246c46947da42ff1be43a48dd5eb6aa
ephysio314 <80077020+ephysio314@users.noreply.github.com>
2025-08-20 11:24:01 +0200

    Add files via upload

---
+++ Code/courses/ansi-c_shell_calculator/0-setup/3/README.md
@@ -0,0 +1,12 @@
+
+VERSION 3
+
+Environnement:
+- Linux
+- apt build-essential
+
+Support:
+- Linux
+- Build static-library .a"
+- Output an executable ".exe" that include the library
+
---
+++ Code/courses/ansi-c_shell_calculator/0-setup/3/src/module_two.c
@@ -0,0 +1,15 @@
+
+float f_add(float _x, float _y){
+	return _x + _y;
+}
+float f_sub(float _x, float _y){
+	return _x - _y;
+}
+float f_mult(float _x, float _y){
+	return _x * _y;
+}
+
+float f_div(float _x, float _y){
+	return _x / _y;
+}
+
